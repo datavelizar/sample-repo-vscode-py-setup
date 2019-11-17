b2fd0986e02acf7cb21eaee017dd8d1d12f87a3a
unknown <velizar_s@mail.bg>
2019-11-17 03:51:45 +0200

    commented some code

--- script.py
+++ script.py
@@ -8,8 +8,8 @@ import requests
 name = input("Your name? ")
 print("Hello, ", name)
 
-print(sys.version)
-print(sys.executable)
+# print(sys.version)
+# print(sys.executable)
 
 
 def greet(who_to_greet):
@@ -19,5 +19,6 @@ def greet(who_to_greet):
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
 print(greet('World'))
 print(greet('Corey'))
